63f720ade711304e6b426a351d121a0e92d6f9a1
Kalle M. Aagaard <git@k-moeller.dk>
2023-12-19 14:32:17 +0100

    init

---
+++ README.md
@@ -0,0 +1,69 @@
+ACMEProxy module for Caddy
+===========================
+
+This package contains a DNS provider module for [Caddy](https://github.com/caddyserver/caddy). It can be used to manage DNS records via an ACMEProxy server.
+
+## Caddy module name
+
+```
+dns.providers.acmeproxy
+```
+
+## Config examples
+
+To use this module for the ACME DNS challenge, [configure the ACME issuer in your Caddy JSON](https://caddyserver.com/docs/json/apps/tls/automation/policies/issuers/acme/) like so:
+
+```json
+{
+	"module": "acme",
+	"challenges": {
+		"dns": {
+			"provider": {
+				"name": "acmeproxy",
+                "username": "user",
+				"password": "pass",
+                "endpoint": "https://example.com:9090"
+			}
+		}
+	}
+}
+```
+
+or with the Caddyfile:
+
+```
+tls {
+	dns acmeproxy https://example.com:9000 {
+        username user
+        password pass
+    }
+}
+```
+
+
+## Authenticating
+
+See [the associated README in the libdns package](https://github.com/libdns/acmeproxy) for important information about credentials.
+
+
+## Troubleshooting
+
+### Error: `timed out waiting for record to fully propagate`
+
+Some environments may have trouble querying the `_acme-challenge` TXT record from dnsproviders. Verify in the providers dashboard that the temporary record is being created.
+
+If the record does exist, your DNS resolver may be caching an earlier response before the record was valid. You can instead configure Caddy to use an alternative DNS resolver such as [Cloudflare's official `1.1.1.1`](https://www.cloudflare.com/en-gb/learning/dns/what-is-1.1.1.1/).
+
+Add a custom `resolver` to the [`tls` directive](https://caddyserver.com/docs/caddyfile/directives/tls):
+
+```
+tls {
+  dns acmeproxy https://example.com:9000 {
+    username user
+    password pass
+  }
+  resolvers 1.1.1.1
+}
+```
+
+Or with Caddy JSON to the `acme` module: [`challenges.dns.provider.resolvers: ["1.1.1.1"]`](https://caddyserver.com/docs/json/apps/tls/automation/policies/issuer/acme/challenges/dns/resolvers/).
---
+++ acmeproxy.go
@@ -0,0 +1,102 @@
+/*
+MIT License
+
+Copyright (c) 2023 Kalle R. Aagaard
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package acmeproxy
+
+import (
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/libdns/acmeproxy"
+)
+
+// Provider wraps the provider implementation as a Caddy module.
+type Provider struct{ *acmeproxy.Provider }
+
+func init() {
+	caddy.RegisterModule(Provider{})
+}
+
+// CaddyModule returns the Caddy module information.
+func (Provider) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "dns.providers.acmeproxy",
+		New: func() caddy.Module { return &Provider{&acmeproxy.Provider{}} },
+	}
+}
+
+// Implements caddy.Provisioner.
+func (p *Provider) Provision(ctx caddy.Context) error {
+	return nil
+}
+
+// UnmarshalCaddyfile sets up the DNS provider from Caddyfile tokens. Syntax:
+//
+//	acmeproxy [<endpoint>] {
+//		endpoint [<endpoint>]
+//	    username <username>
+//	    password <password>
+//	}
+func (p *Provider) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	for d.Next() {
+		if d.NextArg() {
+			p.Provider.Endpoint = d.Val()
+		} else {
+			return d.ArgErr()
+		}
+		if d.NextArg() {
+			return d.ArgErr()
+		}
+		for nesting := d.Nesting(); d.NextBlock(nesting); {
+			switch d.Val() {
+			case "endpoint":
+				if p.Provider.Endpoint != "" {
+					return d.Err("API token already set")
+				}
+				p.Provider.Endpoint = d.Val()
+				if d.NextArg() {
+					return d.ArgErr()
+				}
+			case "username":
+				p.Provider.Username = d.Val()
+				if d.NextArg() {
+					return d.ArgErr()
+				}
+			case "password":
+				p.Provider.Password = d.Val()
+				if d.NextArg() {
+					return d.ArgErr()
+				}
+			default:
+				return d.Errf("unrecognized subdirective '%s'", d.Val())
+			}
+		}
+	}
+	return nil
+}
+
+// Interface guards
+var (
+	_ caddyfile.Unmarshaler = (*Provider)(nil)
+	_ caddy.Provisioner     = (*Provider)(nil)
+)
